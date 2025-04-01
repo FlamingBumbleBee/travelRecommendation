21a7aef15fc722f348b4473a5df61e152210dbb6
FlamingBumbleBee <matt.frerichs@gmail.com>
2025-04-01 16:43:54 -0400

    start on js

---
+++ travel_recommendation.js
@@ -0,0 +1,6 @@
+function searchDest() {
+    const input = document.getElementById('locationInput').value.toLowerCase;
+    const resultDiv = document.getElementById('result');
+
+    resultDiv.innerHTML = '';
+}
