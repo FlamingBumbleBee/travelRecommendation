415ee716af6443358e9a6dba32e383e1f78b90c2
FlamingBumbleBee <matt.frerichs@gmail.com>
2025-04-02 12:20:16 -0400

    getting fetch api working

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,12 +1,31 @@
 function searchDest() {
-    const input = document.getElementById('locationInput').value.toLowerCase;
+    const input = document.getElementById('locationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
-
+    console.log(input);
     resultDiv.innerHTML = '';
 
     fetch('./travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            const data.
+            const countryRec = data.countries.find(item => item.name.toLowerCase() ===input);
+            
+            if (countryRec) {
+                console.log(countryRec.id);
+            }
         })
+        .catch(error => {
+            console.error('Error;', error);
+            resultDiv.innerHTML = 'An error occured while fetching data';
+        });
 }
+
+btnSearch.addEventListener('click', searchDest);
+
+function reset() {
+    const resultDiv = document.getElementById('result');
+
+    resultDiv.innerHTML = ''
+    document.getElementById('locationInput').value = ""
+}
+
+btnReset.addEventListener('click', reset);
