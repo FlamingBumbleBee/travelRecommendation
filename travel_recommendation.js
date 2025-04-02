84d7f055e1e6686996d0afe4346bd04a247d2b21
FlamingBumbleBee <matt.frerichs@gmail.com>
2025-04-02 15:21:33 -0400

    updates through the day

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,16 +1,20 @@
 function searchDest() {
     const input = document.getElementById('locationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
-    console.log(input);
+    //console.log(input);
     resultDiv.innerHTML = '';
 
     fetch('./travel_recommendation_api.json')
         .then(response => response.json())
+
         .then(data => {
-            const countryRec = data.countries.find(item => item.name.toLowerCase() ===input);
-            
-            if (countryRec) {
-                console.log(countryRec.id);
+            console.log(data['countries']);
+            //const travelCategory = data.find(item => )
+            if (input.substring(0,6) === 'countr')  {
+                for (const country of data) {
+                    console.log(data.countries.name);
+                    resultDiv.innerHTML =+ `<h2>${countries.name}</h2>`;
+                }
             }
         })
         .catch(error => {
